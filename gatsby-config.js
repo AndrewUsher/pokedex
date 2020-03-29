8cbffe40cb31faa0e760b2d23eb1dbeb142de355
Unknown <andrewusher00@gmail.com>
2020-03-29 12:54:09 -0500

    chore(config): remove comments_

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-graphql',
       options: {
-        // Arbitrary name for the remote schema Query type
         typeName: 'POKEMON',
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: 'pokemon',
-        // Url to query from
         url: 'https://graphql-pokemon.now.sh/'
       }
     },
