4ce3862a5e26f787f3b65a984f5d374ef0c1800f
Andrew Usher <andrewusher00@gmail.com>
2021-03-14 14:36:23 -0500

    refactor: bring deps up to date :tada:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@drewster'],
+  extends: ['@drewster/eslint-config/react'],
   rules: {
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
