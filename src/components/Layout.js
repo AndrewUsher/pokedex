4fb4c1a460d8bda3752d6c6c9072b6ee526536c7
Andrew Usher <andrewusher00@gmail.com>
2022-05-09 22:57:02 -0500

    feat: add link to homepage

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import * as styles from '../styles/Layout.module.scss'
 
 const Layout = ({ children }) => (
   <>
     <header className={styles.header}>
-      <h1>Pokedex</h1>
+      <h1><Link to="/">Pokedex</Link></h1>
     </header>
     {children}
   </>
